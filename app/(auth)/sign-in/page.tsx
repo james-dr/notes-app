b05fc8b00013f07717bde709a4fdaf9f8a094dc9
James Watson <james.dr.watsn@gmail.com>
2025-05-27 17:20:14 +1000

    Added Google button UI

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -5,8 +5,8 @@ const SignIn = () => {
   const styles = {
     input: {
       shared: "absolute z-0 pointer-events-none transition-all duration-200",
-      offSelect: "left-5 top-4 text-gray-400",
-      onSelect: "left-2 -top-1.5 text-xs font-semi-bold",
+      offSelect: "left-5 top-3 text-gray-400",
+      onSelect: "left-2 -top-2.5 text-xs font-semi-bold",
     },
   };
   const [logInCredentials, setLogInCredentials] = useState({
@@ -47,8 +47,8 @@ const SignIn = () => {
     <div className="flex justify-center items-center h-screen w-screen bg-white">
       <div className="h-fit flex flex-col">
         <h1 className="text-lg font-bold">Sign In</h1>
-        <div className="mt-2">
-          <div className="relative p-2">
+        <div className="my-4">
+          <div className="relative px-2 py-1">
             <input
               className="border rounded-sm m-1 p-1 z-10"
               type="text"
@@ -60,7 +60,7 @@ const SignIn = () => {
             />
             <p className={`${labelStyle.username} ${styles.input.shared}`}>Username</p>
           </div>
-          <div className="relative p-2">
+          <div className="relative px-2 py-1">
             <input
               className="border rounded-sm m-1 p-1 z-10"
               type="password"
@@ -73,7 +73,16 @@ const SignIn = () => {
             <p className={`${labelStyle.password} ${styles.input.shared}`}>Password</p>
           </div>
         </div>
-        <button>Sign in</button>
+        <button className="cursor-pointer bg-gray-400 hover:opacity-80 p-2 font-bold text-sm rounded-sm">Sign in</button>
+        <div className="flex w-full justify-center items-center my-5">
+          <hr className="grow"/>
+          <span className="mx-2 text-xs">or</span>
+          <hr className="grow"/>
+        </div>
+        <button className="hover:bg-black hover:text-white cursor-pointer border-1 p-2 font-bold text-sm rounded-sm flex justify-center items-center gap-2">
+            <img className="w-5 h-5" src={"./google-icon.svg"}/>
+            Sign in with Google
+        </button>
       </div>
     </div>
   );
